97a769ddca2d699920b6e2723ab0e5edf7e4102d
od-ms <wernerth@citeq.de>
2019-06-28 15:04:25 +0000

    Update existing entries, also resources

--- csv2dkan.py
+++ csv2dkan.py
@@ -1,5 +1,5 @@
-import requests
 from contextlib import closing
+import requests
 import csv
 import codecs
 import dkanhandler
@@ -10,13 +10,17 @@ print("Data url:", url)
 
 dkanhandler.connect(cfg)
 
+
 def processDataset(data, resources):
     global dkanhandler, datasets
-    existingDataset = dkanhandler.find(data['name'])
+    if ('nid' in data) and data['nid']:
+        existingDataset = dkanhandler.getDatasetDetails(data['nid'])
+    else:
+        existingDataset = dkanhandler.find(data['name'])
     print()
     print('-----------------------------------------------------')
-    print(data['name'], existingDataset)
-    if (existingDataset):
+    print(data['name'], existingDataset['nid'] if 'nid' in existingDataset else ' => NEW')
+    if existingDataset:
         nid = existingDataset['nid']
         dkanhandler.update(nid, data)
     else:
@@ -29,10 +33,11 @@ def processDataset(data, resources):
     # print(data)
     # print(resources)
 
+
 def updateResources(dataset, resources):
     if (('field_resources' in dataset) and ('und' in dataset['field_resources'])):
         existingResources = dataset['field_resources']['und']
-        if (len(existingResources)):
+        if len(existingResources):
             dkanhandler.updateResources(resources, existingResources, dataset)
     else:
         # Create all resources
@@ -46,20 +51,21 @@ resources = []
 # Read our special CSV format
 # and create or update DKAN entries
 with closing(requests.get(url, stream=True)) as r:
-    reader = csv.reader(codecs.iterdecode(r.iter_lines(), 'utf-8'), delimiter="\t", quotechar='"')
+    reader = csv.reader(codecs.iterdecode(r.iter_lines(), 'utf-8'), delimiter=cfg.csv_separator, quotechar='"')
     for row in reader:
 
         # Handle each row
-        if (row[0]):
-            if ('id' in data):
+        if row[0]:
+            if 'id' in data:
                 processDataset(data, resources)
+                # exit(0)
 
             data = {"id": row[0]}
             resources = []
-        if (row[1]):
+        if row[1]:
             data[row[1]] = row[3]
-        if (row[2]):
-            resources.append({"type": row[2], "url": row[3]})
+        if row[2]:
+            resources.append({"type": row[2], "url": row[3], "title": row[4] if len(row) > 4 else ''})
 
 processDataset(data, resources)
 
--- dkanhandler.py
+++ dkanhandler.py
@@ -1,5 +1,5 @@
 from dkan.client import DatasetAPI
-import argparse
+
 
 def getDkanData(data):
     if not(data['name'] and data['desc'] and data['tags']):
@@ -8,16 +8,16 @@ def getDkanData(data):
     dkanData = {
         "type": "dataset",
         "title": data['name'],
-        "body": {"und":[{"value":data['desc']}]},
-        "field_author": {"und":[{"value":"Stadt Münster"}]},
-        "field_license": {"und":{"select":"Datenlizenz Deutschland – Namensnennung – Version 2.0"}},
-        "field_spatial_geographical_cover": {"und":[{"value":"Münster"}]},
-        "field_granularity": {"und":[{"value":"longitude/latitude"}]},
-        "field_spatial": {"und":{"master_column":"wkt","wkt":"{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[7.5290679931641,51.89293553285],[7.5290679931641,52.007625513725],[7.7350616455078,52.007625513725],[7.7350616455078,51.89293553285]]]},\"properties\":[]}]}"}},
-        "field_tags": {"und":{"value_field": ("\"\"" + data['tags'] +  "\"\"")}},
+        "body": {"und": [{"value": data['desc']}]},
+        "field_author": {"und": [{"value": "Stadt Münster"}]},
+        "field_license": {"und": {"select": "Datenlizenz Deutschland – Namensnennung – Version 2.0"}},
+        "field_spatial_geographical_cover": {"und": [{"value": "Münster"}]},
+        "field_granularity": {"und": [{"value": "longitude/latitude"}]},
+        "field_spatial": {"und": {"master_column": "wkt", "wkt": "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[7.5290679931641,51.89293553285],[7.5290679931641,52.007625513725],[7.7350616455078,52.007625513725],[7.7350616455078,51.89293553285]]]},\"properties\":[]}]}"}},
+        "field_tags": {"und": {"value_field": ("\"\"" + data['tags'] + "\"\"")}},
     }
     if "musterds" in data:
-        dkanData["field_additional_info"] = {"und":[
+        dkanData["field_additional_info"] = {"und": [
             {"first": "Kategorie", "second": data['musterds']},
 
             # TODO => For some reason the second entry does not work!
@@ -26,11 +26,13 @@ def getDkanData(data):
 
     return dkanData
 
+
 def connect(args):
     global api
     print("DKAN url:", args.dkan_url)
 
     api = DatasetAPI(args.dkan_url, args.user, args.passw)
+
 
 def create(data):
     global api
@@ -38,6 +40,7 @@ def create(data):
     res = api.node('create', data=getDkanData(data))
     print("result", res.json())
     return res.json()['nid']
+
 
 def update(nodeId, data):
     global api
@@ -58,33 +61,59 @@ def find(title):
     else:
         return 0
 
+
 def getDatasetDetails(nid):
     global api
     r = api.node('retrieve', node_id=nid)
+    if r.status_code == 404:
+        raise Exception('Did not find existing entry to update:', nid)
+
     return r.json()
 
-def createResource(resource, nid, title):
-    global api
-    print("[create]", resource)
 
+def getResourceDkanData(resource, nid, title):
     rFormat = resource['type']
-    if (rFormat[0:3] == "WFS"): # omit WFS Version in type
+    if (rFormat[0:3] == "WFS"):  # omit WFS Version in type
         rFormat = "WFS"
+
+    rTitle = title + " - " + resource['type']
+    if ('title' in resource) and resource['title']:
+        rTitle = resource['title']
 
     rData = {
         "type": "resource",
         "field_dataset_ref": {"und": [{"target_id": "Name (" + nid + ")"}]},
-        "title": title + " (" + resource['type'] + ")",
+        "title": rTitle,
         "field_link_api": {"und": [{"url": resource['url']}]},
         "field_format": {"und": {"textfield": rFormat}}
     }
-    r = api.node('create', data=rData)
+    return rData
+
+
+def createResource(resource, nid, title):
+    global api
+    print("[create]", resource)
+    r = api.node('create', data=getResourceDkanData(resource, nid, title))
     print(r, r.json())
+
+
+def updateResource(data, nodeId):
+    global api
+    print("[update]", data)
+    r = api.node('update', node_id=nodeId, data=data)
+    if r.status_code != 200:
+        raise Exception('Error during update:', r, r.json())
+    print(r, r.json())
+
 
 def updateResources(resources, existingResources, dataset):
     print("CHECKING RESOURCES")
     for existingResource in existingResources:
+
         print(existingResource['target_id'], end=' ')
+        # TODO: existingResource is crap, because it's only a list of target_ids!
+        # TODO: Don't use existingResource, use resourceData instead!
+
         resourceData = getDatasetDetails(existingResource['target_id'])
         if "und" in resourceData['field_link_api']:
             rUrl = resourceData['field_link_api']['und'][0]['url']
@@ -97,7 +126,13 @@ def updateResources(resources, existingResources, dataset):
         if el:
             # Found url => remove it from the resources that will be created
             resources = [x for x in resources if x['url'] != rUrl]
-            print("[no-change]", rUrl)
+
+            # Check if resource title updated
+            newData = getResourceDkanData(el[0], dataset['nid'], dataset['title'])
+            if newData['title'] != resourceData['title']:
+                updateResource(newData, existingResource['target_id'])
+            else:
+                print("[no-change]", rUrl)
         else:
             # This seems to be an old url that we dont want anymore => delete it
             print("[remove]", existingResource)
